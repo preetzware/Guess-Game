5e3a2c32997bfb85db5131b17c051b0b0b184b41
Preety Ramdin <devianeeramdin@outlook.com>
2024-04-04 20:51:40 +0000

    Add array of objects to js file and rename all 10 images in workspace

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+//Create array of objects for image and answers
+const pictureArray = [
+    picture: 'Mount Fuji',
+        img: 'assets/img/q1.jpeg',
+        answer: 'Japan',
+        options: ['South Korea', 'Japan', 'China']
+    },
+    {
+    picture: 'Iguazufalls',
+        img: 'assets/img/q2.jpg',
+        answer: 'Argentina',
+        options: ['USA', 'Chile', 'Argentina']
+    },
+    {
+    picture: 'Gap-of-Dunloe',
+        img: 'assets/img/q3.jpg',
+        answer: 'Ireland',
+        options: ['England', 'Scotland', 'Ireland']
+    },
+    {
+    picture: 'Golden Temple',
+        img: 'assets/img/q4.png',
+        answer: 'India',
+        options: ['India', 'Thailand', 'Bali']
+    },
+    {
+    picture: 'Mombasa Tusk',
+        img: 'assets/img/q5.jpg',
+        answer: 'Kenya',
+        options: ['Tanzania', 'Kuwait', 'Kenya']
+    },
+    {
+    picture: 'machu-pichu',
+        img: 'assets/img/q6.png',
+        answer: 'Peru',
+        options: ['Mexico', 'Brazil', 'Peru']
+    },
+    {
+    picture: 'under sea waterfall',
+        img: 'assets/img/q7.jpg',
+        answer: 'Mauritius',
+        options: ['Seychelles', 'Maldives Island', 'Mauritius']
+    },
+    {
+    picture: 'Sénanque-Abbey',
+        img: 'assets/img/q8.webp',
+        answer: 'France',
+        options: ['France', 'Spain', 'Italy']
+    },
+    {
+    picture: 'Skogafoss-waterfall',
+        img: 'assets/img/q9.jpg',
+        answer: 'Iceland',
+        options: ['Greenland', 'USA', 'Iceland']
+    },
+    {
+    picture: 'Twelve-Apostles',
+        img: 'assets/img/q10.jpeg',
+        answer: 'Australia',
+        options: ['USA', 'New Zealand', 'Australia']
+    }
+
+];
